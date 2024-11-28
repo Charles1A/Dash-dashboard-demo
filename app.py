25f3255439e77f091a7294ef7d83565c6ae4d8fd
Charles A <93352455+Charles1A@users.noreply.github.com>
2024-11-27 19:13:34 -0600

    Update app.py

--- app.py
+++ app.py
@@ -9,10 +9,12 @@
 import dash
 from dash import Dash, html, dcc, Input, Output, dash_table
 import dash.dash_table.FormatTemplate as FormatTemplate
+import dash_bootstrap_components as dbc
+
 import datetime
 
-import dash_bootstrap_components as dbc
 import plotly.express as px
+import plotly.graph_objects as go
 
 import pandas as pd
 from scipy.stats import pearsonr
